be7352f58582e6fd011bd1df716ff7881e379295
Patrick Gosack <gosack@gmail.com>
2019-08-10 20:39:11 -0700

    made location data local

---
+++ js-client/models/locationData.js
@@ -0,0 +1,140 @@
+module.exports = {
+  1: {
+    location: "JSFNC01A*SG1*IPWSUP",
+    group: "RG3",
+    finger: 12,
+    ceas: [2, 3, 4, 5]
+  },
+  10: {
+    location: "JSFNC01A*SG10*IPWSUP",
+    group: "SDB",
+    finger: 12,
+    ceas: [35, 37, 39, 41]
+  },
+  2: {
+    location: "JSFNC01A*SG2*IPWSUP",
+    group: "SDA",
+    finger: 12,
+    ceas: [6, 8, 10, 12]
+  },
+  3: {
+    location: "JSFNC01A*SG3*IPWSUP",
+    group: "SDA",
+    finger: 12,
+    ceas: [7, 9, 11, 13]
+  },
+  4: {
+    location: "JSFNC01A*SG4*IPWSUP",
+    group: "RG5",
+    finger: 4,
+    ceas: [14, 15, 16, 17]
+  },
+  5: {
+    location: "JSFNC01A*SG5*IPWSUP",
+    group: "RG4",
+    finger: 4,
+    ceas: [18, 19, 20, 21]
+  },
+  6: {
+    location: "JSFNC01A*SG6*IPWSUP",
+    group: "SDB",
+    finger: 12,
+    ceas: [22, 24, 26, 28]
+  },
+  7: {
+    location: "JSFNC01A*SG7*IPWSUP",
+    group: "SDB",
+    finger: 12,
+    ceas: [23, 25, 27, 29]
+  },
+  8: {
+    location: "JSFNC01A*SG8*IPWSUP",
+    group: "PS1",
+    finger: 4,
+    ceas: [30, 31, 32, 33]
+  },
+  9: {
+    location: "JSFNC01A*SG9*IPWSUP",
+    group: "SDB",
+    finger: 12,
+    ceas: [34, 36, 38, 40]
+  },
+  11: {
+    location: "JSFNC01B*SG11*IPWSUP",
+    group: "RG3",
+    finger: 4,
+    ceas: [42, 43, 44, 45]
+  },
+  12: {
+    location: "JSFNC01B*SG12*IPWSUP",
+    group: "PS2",
+    finger: 4,
+    ceas: [46, 48, 50, 52]
+  },
+  13: {
+    location: "JSFNC01B*SG13*IPWSUP",
+    group: "PS2",
+    finger: 4,
+    ceas: [47, 49, 51, 53]
+  },
+  14: {
+    location: "JSFNC01B*SG14*IPWSUP",
+    group: "RG1",
+    finger: 12,
+    ceas: [54, 57, 60, 63]
+  },
+  81: {
+    location: "JSFNC01B*SG-CEA1*IPWSUP",
+    group: "PS1",
+    finger: 4,
+    ceas: [1, null, null, null]
+  },
+  15: {
+    location: "JSFNC01C*SG15*IPWSUP",
+    group: "RG1",
+    finger: 12,
+    ceas: [56, 59, 62, 65]
+  },
+  16: {
+    location: "JSFNC01C*SG16*IPWSUP",
+    group: "SDB",
+    finger: 12,
+    ceas: [55, 58, 61, 64]
+  },
+  17: {
+    location: "JSFNC01C*SG17*IPWSUP",
+    group: "RG2",
+    finger: 4,
+    ceas: [66, 68, 70, 72]
+  },
+  18: {
+    location: "JSFNC01C*SG18*IPWSUP",
+    group: "RG2",
+    finger: 4,
+    ceas: [67, 69, 71, 73]
+  },
+  19: {
+    location: "JSFNC01C*SG19*IPWSUP",
+    group: "SDA",
+    finger: 12,
+    ceas: [74, 76, 78, 80]
+  },
+  20: {
+    location: "JSFNC01C*SG20*IPWSUP",
+    group: "SDA",
+    finger: 12,
+    ceas: [75, 77, 79, 81]
+  },
+  21: {
+    location: "JSFNC01C*SG21*IPWSUP",
+    group: "RG3",
+    finger: 4,
+    ceas: [82, 83, 84, 85]
+  },
+  22: {
+    location: "JSFNC01C*SG22*IPWSUP",
+    group: "RG4",
+    finger: 4,
+    ceas: [86, 87, 88, 89]
+  }
+};
