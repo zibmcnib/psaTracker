b8b2432bcaa867433a4d4aa4a9f3d289ff536662
Patrick Gosack <gosack@gmail.com>
2019-08-10 20:38:09 -0700

    refactor to models

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,31 +1,16 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-var db = mongoose.connection;
-
-db.on('error', function() {
-  console.log('mongoose connection error');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/psaDb", {
+  useMongoClient: true
 });
 
-db.once('open', function() {
-  console.log('mongoose connected successfully');
+const db = mongoose.connection;
+
+db.on("error", function() {
+  console.log("mongoose connection error");
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
+db.once("openUri", function() {
+  console.log("mongoose connected successfully");
 });
 
-var Item = mongoose.model('Item', itemSchema);
-
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
-};
-
-module.exports.selectAll = selectAll;
+module.exports = mongoose;
