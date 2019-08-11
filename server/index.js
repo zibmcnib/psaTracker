6d0a23f0a8324630ce07b731e1a1b47d0e178039
Patrick Gosack <gosack@gmail.com>
2019-08-10 20:38:56 -0700

    built route

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
-let express = require("express");
-// let bodyParser = require("body-parser");
-let items = require("../database-mongo");
+const express = require("express");
+const bodyParser = require("body-parser");
+const findPSA = require("../database-mongo/models/psa");
+const cors = require("cors");
 let app = express();
 
-app.use(express.static(__dirname + "/../react-client/dist"));
+const buildCabinets = require("../js-client/models/buildSystem");
 
-app.get("/items", function(req, res) {
-  items.selectAll(function(err, data) {
-    if (err) {
+app.use(express.static(__dirname + "/../react-client/dist"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/api/psas/:location", (req, res) => {
+  const { location } = req.params;
+  findPSA(location)
+    .then(psa => res.send(psa))
+    .catch(e => {
+      console.error(e);
       res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+    });
+});
+
+app.get("/1", (req, res) => {
+  res.send(buildCabinets(1));
 });
 
 app.listen(3000, function() {
