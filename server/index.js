974b4760434366fe4982c50e351d69c2dc91439f
Patrick Gosack <gosack@gmail.com>
2019-08-11 22:06:50 -0700

    update getCEDMCSStatus route

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const findPSA = require("../database-mongo/models/psa");
 const cors = require("cors");
 let app = express();
 
-const buildCabinets = require("../js-client/models/buildSystem");
+const buildSite = require("../js-client/models/buildSystem");
 
 app.use(express.static(__dirname + "/../react-client/dist"));
 app.use(bodyParser.json());
@@ -13,15 +13,17 @@ app.use(cors());
 app.get("/api/psas/:location", (req, res) => {
   const { location } = req.params;
   findPSA(location)
-    .then(psa => res.send(psa))
+    .then(psa => {
+      res.send(psa);
+    })
     .catch(e => {
       console.error(e);
       res.sendStatus(500);
     });
 });
 
-app.get("/1", (req, res) => {
-  res.send(buildCabinets(1));
+app.get("/api/CEDMCSStatus", (req, res) => {
+  buildSite().then(data => res.send(data));
 });
 
 app.listen(3000, function() {
