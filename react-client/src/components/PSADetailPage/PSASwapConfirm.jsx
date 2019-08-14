daa95cebd84c0ba18d07feae86937b8d9dd77489
Patrick Gosack <gosack@gmail.com>
2019-08-14 01:18:01 -0700

    add notes for when I pick this up again

--- react-client/src/components/PSADetailPage/PSASwapConfirm.jsx
+++ react-client/src/components/PSADetailPage/PSASwapConfirm.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import states from "../states";
 
-const PSASwapConfirm = ({ spares, onPSAClick, changeViewState }) => {
+/// Where you left off...
+
+// You currently have access to the psa selected from the landing page and the psa selected
+// from the spare pool. Now you just need to use your CRUD routes to perform the swap.
+
+const PSASwapConfirm = ({ state, onPSAClick, changeViewState }) => {
   return (
     <div className="backdrop">
       <div className="modal">
         <button onClick={() => changeViewState(states.detailView)}>
-          {this.state.selectedPSA}
+          {console.log(state)}
         </button>
       </div>
     </div>
