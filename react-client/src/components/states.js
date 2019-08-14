01833a5798ba053d26068432bdff4b18b03c8501
Patrick Gosack <gosack@gmail.com>
2019-08-14 00:58:50 -0700

    massive update for state handling

---
+++ react-client/src/components/states.js
@@ -0,0 +1,50 @@
+module.exports = {
+  isLoading: {
+    isLoading: true,
+    landing: false,
+    detailView: false,
+    spareView: false,
+    swapPSAInitialView: false,
+    swapPSAConfirmView: false
+  },
+  landing: {
+    isLoading: false,
+    landing: true,
+    detailView: false,
+    spareView: false,
+    swapPSAInitialView: false,
+    swapPSAConfirmView: false
+  },
+  detailView: {
+    isLoading: false,
+    landing: false,
+    detailView: true,
+    spareView: false,
+    swapPSAInitialView: false,
+    swapPSAConfirmView: false
+  },
+  spareView: {
+    isLoading: false,
+    landing: false,
+    detailView: false,
+    spareView: true,
+    swapPSAInitialView: false,
+    swapPSAConfirmView: false
+  },
+  swapPSAInitialView: {
+    isLoading: false,
+    landing: false,
+    detailView: false,
+    spareView: false,
+    swapPSAInitialView: true,
+    swapPSAConfirmView: false
+  },
+  swapPSAConfirmView: {
+    isLoading: false,
+    landing: false,
+    detailView: false,
+    spareView: false,
+    swapPSAInitialView: false,
+    swapPSAConfirmView: true
+  }
+};
